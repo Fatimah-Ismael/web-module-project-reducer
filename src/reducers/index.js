44b5090e0670cfc600343be387d5ee4e7eb8afff
Fatimah Ismael <fatimahismael@yahoo.com>
2023-01-20 10:52:15 -0800

    buttons all working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_CE } from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -37,6 +37,11 @@ const reducer = (state, action) => {
                 operation: action.payload
             });
             
+        case(CLEAR_CE):
+            return({
+                ...state, 
+                total: 0
+            });
         default:
             return state;
     }
